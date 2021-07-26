6ee7a148081f0230015a80cb3917ac56661a93f9
Siva Nookala <nookala.sivakumar@gmail.com>
2021-07-26 22:41:57 +0530

    Jokes App project creation.closes #1

---
+++ 4-jokes-app/src/test/java/com/javablackboard/demo/jokes/app/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javablackboard.demo.jokes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
